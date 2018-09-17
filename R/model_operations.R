3423c40392bb180fbdfdefa84b131e14b5cf04fc
alex23lemm <alex23lemm@gmail.com>
2018-09-17 22:22:40 +0200

    Initial commit

---
+++ R/model_operations.R
@@ -0,0 +1,83 @@
+#' List available models
+#'
+#' Retrieves model names of all available PMML models on ZEMENTIS Server.
+#'
+#' @return A character vector that lists all available PMML models on ZEMENTIS
+#'   Server.
+#' @importFrom magrittr %>%
+#' @export
+get_models <- function() {
+
+  url <- paste(get_zementis_base_url(), "models", sep = "/")
+
+  response <- httr::GET(url, httr::authenticate(get_zementis_usr(),
+                                                get_zementis_pwd()),
+                        httr::user_agent(get_useragent()))
+
+  if (httr::http_error(response)) {
+    stop(
+      sprintf(
+        "Zementis Server API request failed [%s]\n%s\n%s\n%s",
+        httr::status_code(response),
+        httr::http_status(response)$category,
+        httr::http_status(response)$reason,
+        httr::http_status(response)$message
+      ),
+      call. = FALSE
+    )
+  }
+
+  if (httr::http_type(response) != "application/json") {
+    stop("Zementis Server API did not return json", .call = FALSE)
+  }
+
+  httr::content(response, as = "text") %>%
+    jsonlite::fromJSON() %>%
+    unlist(use.names = FALSE)
+}
+
+#' Activate existing PMML model
+#'
+#' Activates an existing PMML model which was deployed to ZEMENTIS Server.
+#'
+#' @param name The name of the model that is activated on ZEMENTIS server.
+#' @return A data frame that contains a subset of the model's properties including
+#' its name and active status if the activation on the server was successfull.
+#' Otherwise an error is thrown.
+#' @importFrom magrittr %>%
+#' @export
+activate_model <- function(name) {
+
+  url <- paste(get_zementis_base_url(), "model", name, "activate",
+               sep = "/")
+
+  response <- httr::PUT(url, httr::authenticate(get_zementis_usr(),
+                                                get_zementis_pwd()),
+                        httr::user_agent(get_useragent()))
+
+  if (httr::http_error(response)) {
+    error_message <- sprintf(
+      "Zementis Server API request failed [%s]\n%s\n%s\n%s",
+      httr::status_code(response),
+      httr::http_status(response)$category,
+      httr::http_status(response)$reason,
+      httr::http_status(response)$message
+    )
+    if(status_code(response) == 404) {
+      error_message <- paste(error_message,
+                             httr::content(response)$errors[[1]],
+                             sep = "\n")
+    }
+
+
+    stop(error_message, call. = FALSE)
+  }
+  parsed <- httr::content(response, as = "text") %>%
+    jsonlite::fromJSON()
+
+  tibble::data_frame(
+    model_name = parsed[["modelName"]],
+    is_active = parsed[["isActive"]]
+  )
+}
+
---
+++ R/utils.R
@@ -0,0 +1,30 @@
+
+
+get_useragent <- function() {
+  ua <- "Request created by the zementisr package; https://github.com/alex23lemm/zementisr"
+  ua
+}
+
+get_zementis_base_url <- function() {
+  val <- Sys.getenv("ZEMENTIS_base_url")
+  if (identical(val, "")) {
+    stop("`ZEMENTIS_base_url` env var has not been set.")
+  }
+  val
+}
+
+get_zementis_usr <- function() {
+  val <- Sys.getenv("ZEMENTIS_usr")
+  if (identical(val, "")) {
+    stop("`ZEMENTIS_usr` env var has not been set.")
+  }
+  val
+}
+
+get_zementis_pwd <- function() {
+  val <- Sys.getenv("ZEMENTIS_pwd")
+  if (identical(val, "")) {
+    stop("`ZEMENTIS_pwd` env var has not been set.")
+  }
+  val
+}
