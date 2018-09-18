2d82f4fad50d3af303853ba5f30d725c447f3dbb
alex23lemm <alex23lemm@gmail.com>
2018-09-18 23:08:48 +0200

    Add upload_model()

--- R/model_operations.R
+++ R/model_operations.R
@@ -1,8 +1,8 @@
 #' List available models
 #'
-#' Retrieves model names of all available PMML models on ZEMENTIS Server.
+#' Retrieves model names of all available PMML models on Zementis Server.
 #'
-#' @return A character vector that lists all available PMML models on ZEMENTIS
+#' @return A character vector that lists all available PMML models on Zementis
 #'   Server.
 #' @export
 get_models <- function() {
@@ -33,17 +33,96 @@ get_models <- function() {
     unlist(use.names = FALSE)
 }
 
+
+#' Upload new PMML model
+#'
+#' Upload new PMML source or serialized model file to Zementis Server.
+#'
+#' @param file Path to a file. The PMML model file can end in \code{.xml},
+#' \code{.pmml} or even be \code{.zip} or \code{.gzip} compressed.
+#' @param applyCleanser Logical indicating if the server should perfom cleansing
+#'  on the PMML file. (Default: TRUE)
+#' @return  If a model with the same name already existis on the server or if is
+#'  not valid PMML, an error.
+#' Otherwise if is a valid PMML model, a list with the following components:
+#'  \item{model_name}{The name of the uploaded model}
+#'  \item{is_active}{A logical indicating the activation status of the model
+#'    which is TRUE after the initial model upload.}
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'
+#'    #Build a simple lm model
+#'    iris_lm <- lm(Sepal.Length ~ ., data=iris)
+#'    # Convert to pmml and save to disk
+#'    iris_pmml <- pmml(iris_lm)
+#'    saveXML(iris_pmml, "iris_pmml.xml")
+#'
+#'    # Upload model to server
+#'    upload_model("iris_pmml.xml")
+#'  }
+upload_model <- function(file, applyCleanser = TRUE) {
+
+  if (!file.exists(file)) {
+    stop("Please provide a valid path to the model file.", .call = FALSE)
+  }
+  applyCleanser <- ifelse(applyCleanser, "true", "false")
+  url <- paste(get_zementis_base_url(), "model" , sep = "/") %>%
+    modify_url(query = list(applyCleanser = applyCleanser))
+  my_file <- upload_file(file)
+
+  response <- httr::POST(url, httr::authenticate(get_zementis_usr(),
+                                                 get_zementis_pwd()),
+                         httr::user_agent(get_useragent()),
+                         body = list(file = my_file))
+
+  if (httr::status_code(response) != 201) {
+    error_message <- sprintf(
+      "Zementis Server API request failed [%s]\n%s\n%s\n%s",
+      httr::status_code(response),
+      httr::http_status(response)$category,
+      httr::http_status(response)$reason,
+      httr::http_status(response)$message
+    )
+    if (httr::status_code(response) %in% c(400, 409)) {
+      error_message <- paste(error_message,
+                             content(response)$errors[[1]],
+                             sep = "\n")
+    }
+    stop(error_message, call. = FALSE)
+  }
+  if (httr::http_type(response) != "application/json") {
+    stop("Zementis Server API did not return json", .call = FALSE)
+  }
+  parsed <- httr::content(response, as = "text") %>%
+    jsonlite::fromJSON()
+
+  list(
+    model_name = parsed[["modelName"]],
+    is_active = parsed[["isActive"]]
+  )
+}
+
+
 #' Activate existing PMML model
 #'
-#' Activates an existing PMML model which was deployed to ZEMENTIS Server.
+#' Activates an existing PMML model which was deployed to Zementis Server.
 #'
-#' @param name The name of the model that is activated on ZEMENTIS server.
+#' @param name The name of the model that is activated on Zementis server.
 #' @return If the model name is not known to the server, an error. Otherwise a
 #'  list with components:
-#'  \item{model_name}{The name of the activated model}
-#'  \item{is_active}{The activation status of the model}
+#'  \item{model_name}{The \code{name} of the activated model}
+#'  \item{is_active}{A logical indicating the activation status of the model}
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#'   activate_model("IrisMLRModel")
+#' }
 activate_model <- function(name) {
+
+  stopifnot(is.character(name))
 
   url <- paste(get_zementis_base_url(), "model", name, "activate",
                sep = "/")
@@ -60,7 +139,7 @@ activate_model <- function(name) {
       httr::http_status(response)$reason,
       httr::http_status(response)$message
     )
-    if(status_code(response) == 404) {
+    if(httr::status_code(response) == 404) {
       error_message <- paste(error_message,
                              httr::content(response)$errors[[1]],
                              sep = "\n")
@@ -79,4 +158,3 @@ activate_model <- function(name) {
     is_active = parsed[["isActive"]]
   )
 }
-
