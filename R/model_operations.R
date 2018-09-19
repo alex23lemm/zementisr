afab974a6e44b7559b7d6086b70b8b1a1133fa01
alex23lemm <alex23lemm@gmail.com>
2018-09-19 22:38:56 +0200

    Add delete_model()

--- R/model_operations.R
+++ R/model_operations.R
@@ -30,7 +30,7 @@ get_models <- function() {
   }
   httr::content(response, as = "text") %>%
     jsonlite::fromJSON() %>%
-    unlist(use.names = FALSE)
+    purrr:::flatten_chr()
 }
 
 
@@ -56,7 +56,7 @@ get_models <- function() {
 #'    #Build a simple lm model
 #'    iris_lm <- lm(Sepal.Length ~ ., data=iris)
 #'    # Convert to pmml and save to disk
-#'    iris_pmml <- pmml(iris_lm)
+#'    iris_pmml <- pmml(iris_lm, model.name = "iris_model")
 #'    saveXML(iris_pmml, "iris_pmml.xml")
 #'
 #'    # Upload model to server
@@ -105,26 +105,79 @@ upload_model <- function(file, applyCleanser = TRUE) {
 }
 
 
+#' Delete model
+#'
+#' Remove model from server and list remaining models.
+#'
+#' @param name Name of the model to be deleted.
+#' @return If deletion is successful, a vector of names of models remaining
+#' deployed on the server with \code{model_name} missing in it. Otherwise, an
+#' error.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  delete("iris_model")
+#'
+#'  #Delete all models
+#'  get_models() %>% purrr:::map_chr(delete_model)
+#'  }
+delete_model <- function(model_name) {
+
+  stopifnot(is.character(model_name))
+
+  url <- paste(get_zementis_base_url(), "model", gsub(" ", "%20", model_name),
+               sep = "/")
+
+  response <- httr::DELETE(url, httr::authenticate(get_zementis_usr(),
+                                                get_zementis_pwd()),
+                        httr::user_agent(get_useragent()))
+
+  if (httr::http_error(response)) {
+    error_message <- sprintf(
+      "Zementis Server API request failed [%s]\n%s\n%s\n%s",
+      httr::status_code(response),
+      httr::http_status(response)$category,
+      httr::http_status(response)$reason,
+      httr::http_status(response)$message
+    )
+    if(httr::status_code(response) == 404) {
+      error_message <- paste(error_message,
+                             httr::content(response)$errors[[1]],
+                             sep = "\n")
+    }
+    stop(error_message, call. = FALSE)
+  }
+
+  if (httr::http_type(response) != "application/json") {
+    stop("Zementis Server API did not return json", .call = FALSE)
+  }
+  httr::content(response, as = "text") %>%
+    jsonlite::fromJSON() %>%
+    purrr:::flatten_chr()
+}
+
+
 #' Activate existing PMML model
 #'
 #' Activates an existing PMML model which was deployed to Zementis Server.
 #'
-#' @param name The name of the model that is activated on Zementis server.
+#' @param model_name The name of the model that is activated on Zementis server.
 #' @return If the model name is not known to the server, an error. Otherwise a
 #'  list with components:
-#'  \item{model_name}{The \code{name} of the activated model}
+#'  \item{model_name}{The \code{model_name} of the activated model}
 #'  \item{is_active}{A logical indicating the activation status of the model}
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#'   activate_model("IrisMLRModel")
+#'   activate_model("iris_model")
 #' }
-activate_model <- function(name) {
+activate_model <- function(model_name) {
 
-  stopifnot(is.character(name))
+  stopifnot(is.character(model_name))
 
-  url <- paste(get_zementis_base_url(), "model", gsub(" ", "%20", name),
+  url <- paste(get_zementis_base_url(), "model", gsub(" ", "%20", model_name),
                "activate", sep = "/")
 
   response <- httr::PUT(url, httr::authenticate(get_zementis_usr(),
@@ -163,22 +216,22 @@ activate_model <- function(name) {
 #'
 #' Dectivates an existing PMML model which was deployed to Zementis Server.
 #'
-#' @param name The name of the model that is deactivated on Zementis server.
+#' @param model_name The name of the model that is deactivated on Zementis server.
 #' @return If the model name is not known to the server, an error. Otherwise a
 #'  list with components:
-#'  \item{model_name}{The \code{name} of the deactivated model}
+#'  \item{model_name}{The \code{model_name} of the deactivated model}
 #'  \item{is_active}{A logical indicating the activation status of the model}
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#'   deactivate_model("IrisMLRModel")
+#'   deactivate_model("iris_model")
 #' }
-deactivate_model <- function(name) {
+deactivate_model <- function(model_name) {
 
-  stopifnot(is.character(name))
+  stopifnot(is.character(model_name))
 
-  url <- paste(get_zementis_base_url(), "model", gsub(" ", "%20", name),
+  url <- paste(get_zementis_base_url(), "model", gsub(" ", "%20", model_name),
                "deactivate", sep = "/")
 
   response <- httr::PUT(url, httr::authenticate(get_zementis_usr(),
