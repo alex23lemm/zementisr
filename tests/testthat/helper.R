0db087ff4612d0188295059be576a22ac15985ae
alex23lemm <alex23lemm@gmail.com>
2018-10-02 00:35:28 +0200

    Add helper.R to test folder

---
+++ tests/testthat/helper.R
@@ -0,0 +1,15 @@
+# Create new PMML models which will be deployed to Zementis Server
+data(kyphosis, package = "rpart")
+kyphosis_fit <- rpart::rpart(Kyphosis ~ Age + Number + Start, data = rpart::kyphosis)
+kyphosis_pmml <- pmml::pmml(kyphosis_fit, model.name = "kyphosis_model")
+XML::saveXML(kyphosis_pmml, "kyphosis_pmml.xml")
+
+iris_fit <- lm(Sepal.Length ~ ., data=iris)
+iris_pmml <- pmml::pmml(iris_fit, model.name = "iris_model")
+XML::saveXML(iris_pmml, "iris_pmml.xml")
+
+err_not_known <- paste("Zementis Server API request failed [404]",
+                       "Client error",
+                       "Not Found",
+                       "Client error: (404) Not Found",
+                       "Model 'unknown_model' not found.", sep = "\n")
