7ccc856ca489fc1ecb5723fe25f492a648ee1f26
alex23lemm <alex23lemm@gmail.com>
2018-10-03 02:31:51 +0200

    Create first version of apply_model_batch()

--- R/apply.R
+++ R/apply.R
@@ -1,5 +1,3 @@
-#' Apply PMML model to data
-#'
 #' Apply model to single input record
 #'
 #' @param x A one row data frame containg the data set which will be send to
@@ -18,8 +16,8 @@
 #'  the predicted value.
 #'
 #'  For binary classification models \code{outputs} will include a 3-column
-#'  data frame that includes the probability of class A, the probability of
-#'  class B and the classification class label result based on a 50\% threshold.
+#'  data frame that includes the probability of class 0, the probability of
+#'  class 1 and the classification class label result based on a 50\% threshold.
 #' @seealso \code{\link{upload_model}}
 #' @export
 #'
@@ -28,8 +26,11 @@
 #' \dontrun{
 #' apply_model("iris_model", iris[1, ])
 #' }
+apply_model <- function(x, model_name, ...) {
 
-apply_model <- function(model_name, x, ...) {
+  if(dim(x)[1] > 1) {
+    stop("Please provide a data frame with a single record.", .call = FALSE)
+  }
 
   url <- paste(get_zementis_base_url(),
                "apply",
@@ -60,4 +61,34 @@ apply_model <- function(model_name, x, ...) {
   }
   httr::content(response, as = "text") %>%
    jsonlite::fromJSON()
+}
+
+#' Apply model to multiple input records
+#'
+#' @param df A data frame containg the data sets which will be send to
+#'  Zementis Server for prediction. The data frame column names must match
+#'  the PMML model argument names.
+#' @param model_name The name of the deployed PMML model which will predict
+#'  the records \code{df}.
+#' @inheritParams get_models
+#'
+#' @export
+apply_model_batch <- function(file, model_name) {
+
+  # temp <- tempfile(fileext = ".json")
+  # on.exit(unlink(temp))
+  # write_json(df, temp)
+
+  url <- paste(get_zementis_base_url(), "apply" , model_name,  sep = "/")
+
+  my_file <- httr::upload_file(file)
+  response <- httr::POST(url, httr::authenticate(get_zementis_usr(),
+                                                 get_zementis_pwd()),
+                         accept_json(),
+                         httr::user_agent(get_useragent()),
+                         body = list(file = my_file))
+  #httr::content(response, as = "text") %>% jsonlite::fromJSON()
+response
+  # immmer json like zurückgeben, egal welcher input ABER: csv-daten müssen
+  # entsprechend in liste gebracht werden :-(
 }
--- tests/testthat/test-02-apply_model.R
+++ tests/testthat/test-02-apply_model.R
@@ -1,4 +1,4 @@
-context("02-apply_model")
+context("apply model to data check")
 
 test_that("apply_model() returns correct prediction for regression models", {
   expect_equivalent(apply_model("iris_model", iris[42, ])$outputs[1, ],
@@ -20,3 +20,19 @@ test_that("apply_model() returns correct prediction for classififcation models",
                  sort)
 })
 
+test_that("apply_model() returns correct prediction for binary classififcation models", {
+  server_prediction <- apply_model("kyphosis_model", kyphosis[38, ])
+
+  expect_equal(server_prediction$outputs[, 3],
+               as.character(predict(kyphosis_fit, kyphosis[38, ], type = "class")))
+  expect_equal(server_prediction$outputs[, 1:2] %>%
+                 as.double %>%
+                 sort,
+               predict(kyphosis_fit, newdata = kyphosis[38, ]) %>%
+                 as.double %>%
+                 sort)
+})
+
+test_that("apply_model() returns error if model name is unknown to the server", {
+  expect_error(apply_model("unknown_model", c(1:3)), err_not_known, fixed = TRUE)
+})
